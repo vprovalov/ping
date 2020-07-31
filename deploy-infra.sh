9cba34dd83cd86b04909e77b719f42afe09882df
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-07-31 00:01:12 -0700

    rename to yml

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,10 +5,16 @@ REGION=us-east-2
 CLI_PROFILE=aws
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile aws \
     --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# Generate a personal access token with repo and admin:repo_hook 
+# permissions from https://github.com/settings/tokens 
+GH_ACCESS_TOKEN=$(cat ~/.github/ping-access-token) 
+GH_OWNER=$(cat ~/.github/ping-owner)
+GH_REPO=$(cat ~/.github/ping-repo) 
+GH_BRANCH=master
 
 # Deploys static resources
 echo -e "\n\n=========== Deploying setup.yml ===========" 
@@ -28,11 +34,16 @@ aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
     --stack-name $STACK_NAME \
-    --template-file main.yaml \
+    --template-file main.yml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
  # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
