910cc035c789f5997cf7585812fe3cfe3436d4c4
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-07-31 00:19:27 -0700

    fix code deploy

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,4 @@
  #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/app/bin
-./server & echo $! > /home/ec2-user/app/server.pid
+cd /home/ec2-user/app/release/bin
+./server & echo $! > /home/ec2-user/app/release/server.pid
--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,5 @@
  #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -f "/home/ec2-user/app/server.pid" ] && \
-kill `cat /home/ec2-user/app/server.pid` && \
-rm /home/ec2-user/app/server.pid
+[ -f "/home/ec2-user/app/release/server.pid" ] && \
+kill `cat /home/ec2-user/app/release/server.pid` && \
+rm /home/ec2-user/app/release/server.pid
