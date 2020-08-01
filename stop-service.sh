18b4435a16a9fa10696291c9bda2952446719f98
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-08-01 16:49:09 -0700

    fix start/stop script

--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,5 @@
  #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -f "/home/ec2-user/app/release/server.pid" ] && \
-kill `cat /home/ec2-user/app/release/server.pid` && \
-rm /home/ec2-user/app/release/server.pid
+[ -f "/tmp/server.pid" ] && \
+kill `cat /tmp/server.pid` && \
+rm /tmp/server.pid
