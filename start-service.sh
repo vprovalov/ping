ddb9c998b552b4a5ddd4031b6d33445690ebb321
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-08-01 18:36:38 -0700

    introduce process manager github.com/struCoder/pmgo

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,4 @@
  #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/app/release/bin
-./server & echo $! > /tmp/server.pid
+
+/home/ec2-user/go/bin/pmgo start /home/ec2-user/app/release/bin/server server true
--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,4 @@
  #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-[ -f "/tmp/server.pid" ] && \
-kill `cat /tmp/server.pid` && \
-rm /tmp/server.pid
+
+/home/ec2-user/go/bin/pmgo stop server
