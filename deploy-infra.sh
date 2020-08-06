0665c54f89682e0298007dcd61d803678a9d6c71
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-08-05 23:34:08 -0700

    add second instance

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -48,6 +48,6 @@ aws cloudformation deploy \
  # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
-    --profile $CLI_PROFILE \
-    --query "Exports[?Name=='InstanceEndpoint'].Value"
+        --profile $CLI_PROFILE \
+        --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
