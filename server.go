6642ae657225e0d26ba1c3c1be5e3e09d5939ee1
Valeriy Provalov <valeriy.provalov@gmail.com>
2020-08-12 22:19:49 -0700

    added load balancer

--- server.go
+++ server.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+var hostname, _ = os.Hostname()
+
+func index(c *gin.Context) {
+	c.String(http.StatusOK, "")
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message":  "pong",
+		"hostname": hostname,
+	})
+}
+
 func main() {
 	r := gin.Default()
-	var hostname, _ = os.Hostname()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message":  "pong",
-			"hostname": hostname,
-		})
-	})
+
+	r.GET("/", index)
+	r.GET("/ping", ping)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
